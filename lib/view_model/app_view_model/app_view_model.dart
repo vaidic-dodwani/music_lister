f5dc996abe30543b07c2ef3a10ca16b7cf63072a
vaidic-dodwani <vaidicdodwani@gmail.com>
2023-10-27 12:40:42 +0530

    Edit

--- lib/view_model/app_view_model/app_view_model.dart
+++ lib/view_model/app_view_model/app_view_model.dart
@@ -2,30 +2,30 @@ import 'package:flutter/material.dart';
 import 'package:music_lister/model/playlist_model.dart';
 
 class AppNotifier extends ChangeNotifier {
-  LocalSongLists? _localData;
+  LocalSongLists _localData = LocalSongLists(playlists: []);
 
-  LocalSongLists? get localData => _localData;
-  set localData(LocalSongLists? _) {
+  LocalSongLists get localData => _localData;
+  set localData(LocalSongLists _) {
     _localData = _;
     notifyListeners();
   }
 
   void addPlaylist(Playlist playlist) {
-    _localData?.playlists.add(playlist);
+    _localData.playlists.add(playlist);
     notifyListeners();
   }
 
   void removePlaylist(Playlist playlist) {
-    _localData?.playlists.remove(playlist);
+    _localData.playlists.remove(playlist);
     notifyListeners();
   }
 
   void editPlaylist(Playlist playlist, int index) {
-    if (_localData != null) {
+    if (_localData.playlists.length > index) {
       final temp = _localData;
-      temp!.playlists.removeAt(index);
+      temp.playlists.removeAt(index);
       temp.playlists.insert(index, playlist);
+      notifyListeners();
     }
-    notifyListeners();
   }
 }
